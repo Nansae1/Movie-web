a8363ef2bb7026e6bc60a856c05718068f6521d9
25LP4194 25LP4194 <25LP4194@pinecones-iMac.local>
2025-11-17 15:05:52 +0800

    new

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 
 import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
-import { Select } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import {
   Carousel,
@@ -15,6 +14,10 @@ import {
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { MovieSection } from "./_components/MovieSection";
+import { Popover } from "@radix-ui/react-popover";
+import { PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Badge } from "@/components/ui/badge";
+import { ChevronDown, Search } from "lucide-react";
 
 const slideimg = ["/wicked.jpg", "/movie2.png", "/movie3.jpg"];
 
@@ -173,8 +176,111 @@ export default function Home() {
         <div className="flex w-full justify-between max-w-360 h-15 items-center ">
           <img src="./Logo.png" className="h-5 w-23"></img>
           <div className="flex gap-3 items-center">
-            <Select className="h-9 w-24.25">Genre</Select>
-            <Input className="h-9 w-94.75" placeholder="Search.."></Input>
+            <Popover>
+              <PopoverTrigger
+                className="h-9 w-24 border border-[#E4E4E7] rounded-md"
+                asChild
+              >
+                <Button variant="secondary">
+                  <ChevronDown />
+                  Genre
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-144.25 h-83.25">
+                <div className="flex flex-col gap-1 w-134.25 border-b pb-4 border-b-[#E4E4E7]">
+                  <p className="text-[24px] font-semibold">Genres</p>
+                  <p>See lists of movies by genre</p>
+                </div>
+                <div className="w-full flex flex-wrap gap-4 my-4">
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Action
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Adventure
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Animation
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Biography
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Comedy
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Crime
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Documentary
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Drama
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Family
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Fantasy
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Film-Noir
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Game-Show
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    History
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Horror
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Music
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Musical
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Mystery
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    News
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Reality-TV
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Romance
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Sci-Fi
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Short
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Sport
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Talk-Show
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Thriller
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    War
+                  </Badge>
+                  <Badge className="bg-white text-black px-2 border border-[#E4E4E7] text-xs">
+                    Western
+                  </Badge>
+                </div>
+              </PopoverContent>
+            </Popover>
+
+            <div className=" relative flex items-center">
+              <Search className="absolute  left-3 w-4 h-4" />
+              <Input className="h-9 w-94.75 pl-10" placeholder="Search.." />
+            </div>
           </div>
           <img src="./Modes.png" className="h-9 w-9"></img>
         </div>
@@ -229,26 +335,26 @@ export default function Home() {
           );
         })}
       </div>
-      <div className="w-full max-w-360 h-70 bg-[#4338CA] flex gap-122.25 my-7">
+      <div className="py-10 px-15 w-full max-w-360 bg-[#4338CA] flex gap-122.25 my-7">
         <div className="flex flex-col gap-3">
-          <div className="flex">
+          <div className="flex gap-2 items-center">
             <img src="./Vector (7).png" className="h-5 w-5"></img>
             <p className="text-white">Movie Z</p>
           </div>
           <p className="text-white">© 2024 Movie Z. All Rights Reserved.</p>
         </div>
         <div className="flex gap-24">
-          <div className="flex flex-col">
+          <div className="flex flex-col pb-16">
             <div className="text-white">Contact Information</div>
-            <div className="flex">
-              <img className="" src="./Wifi icon.png"></img>
+            <div className="flex gap-3 items-center">
+              <img className="h-4 w-4" src="./Wifi icon.png"></img>
               <div className="flex flex-col">
                 <p className="text-white">Email:</p>
                 <p className="text-white">support@movieZ.com</p>
               </div>
             </div>
-            <div>
-              <img></img>
+            <div className="flex  gap-3 items-center">
+              <img src="./Wifi icon (1).png" className="h-4 w-4"></img>
               <div className="flex flex-col">
                 <p className="text-white">Phone:</p>
                 <p className="text-white">+976 (11) 123-4567</p>
@@ -257,7 +363,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col">
             <div className="text-white">Follow us </div>
-            <div className="flex">
+            <div className="flex gap-3">
               <p className="text-white">Facebook</p>
               <p className="text-white">Instagram</p>
               <p className="text-white">Twitter</p>
